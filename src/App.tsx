004e377fa5db4f1bcb5c8b29d3003022db1226da
Devin Kelly-Collins <devin.kellycollins@artisantechgroup.com>
2019-07-12 08:57:22 -0500

    Installed ionic framework.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { IonApp, IonContent, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IonApp>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Welcome to Ionic</IonCardSubtitle>
+            <IonCardTitle>Running on React</IonCardTitle>
+          </IonCardHeader>
+        </IonCard>
+      </IonContent>
+    </IonApp>
   );
 }
 
